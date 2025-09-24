7f1721e709db1746aa10c3ddaf6db852a9d7f856
Shimon Schlessinger <sschlessinger@hwemail.com>
2025-09-24 12:53:11 -0700

    (GP-2.1): Program Can Initialize Repository

--- Git.java
+++ Git.java
@@ -1,5 +1,52 @@
+import java.io.File;
+
 public class Git {
+    public static boolean directoryExists(String filePath, String folderName){
+        String path = (filePath == null ? "" : filePath + "/") + folderName;
+        File newDir = new File(path);
+        return newDir.exists();
+    }
+
+    public static boolean fileExists(String filePath, String fileName){
+        String path = (filePath == null ? "" : filePath + "/") + fileName;
+        File f = new File(path);
+        return f.exists();
+    }
+
+    public static void makeDirectory(String filePath, String folderName){
+        if (directoryExists(filePath, folderName)){
+            return;
+        }
+        String path = (filePath == null ? "" : filePath + "/") + folderName;
+        File newDir = new File(path);
+        newDir.mkdir();
+    }
+
+    public static void makeFile(String filePath, String fileName){
+        if (fileExists(filePath, fileName)){
+            return;
+        }
+        String path = (filePath == null ? "" : filePath + "/") + fileName;
+        File f = new File(path);
+        try {
+            f.createNewFile();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    public static void intializeRepo(){
+        if (fileExists("git/objects", "index") && fileExists("git/objects", "HEAD")){
+            System.out.println("Git Repository Already Exists");
+            return;
+        }
+        makeDirectory(null, "git");
+        makeDirectory("git", "objects");
+        makeFile("git/objects", "index");
+        makeFile("git/objects", "HEAD");
+        System.out.println("Git Repository Created");
+    }
     public static void main(String[] args){
-        
+        intializeRepo();
     }
 }
---
+++ README.md
@@ -0,0 +1,7 @@
+Directory Exists: Takes a file path and the directory name and checks existence
+File Exists: Ditto with files of directories
+
+Make Directory: Takes a file path and the directory and makes it IF not already there
+Make File: Ditto with files instead of directory
+
+Initialize Repo: If they don't exist, makes a "git" directory containing an "objects" direcotry with two files "index" and "HEAD." If they all already exists, it logs "Git Repository Already Exists" 
